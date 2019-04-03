28fbdb34289e378af5192a83b912d2400da6bb10
Fernando Macedo <fermaccha@gmail.com>
2019-04-03 10:42:22 -0400

    Se aplicado, este commit tera disponível a funcionalidade de cadastrar usuários, a view 'application.html.erb' contera 'flash-messages'

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Usuário cadastrado com sucesso"
+      redirect_to root_url
+    else
+      render "new"
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+end
